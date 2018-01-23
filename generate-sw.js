393e24a3fc3efdb563af27bed1fa1c12cef83d4f
tennosys <tennosys@gmail.com>
2018-01-23 16:02:24 +0700

    fix() fix sw add no script

--- generate-sw.js
+++ generate-sw.js
@@ -5,6 +5,10 @@ workboxBuild.generateSW({
   globPatterns: ['**\/*.{html,js,css}'],
   globIgnores: ['**/sw.js'],
   swDest: './build/sw.js',
+  navigateFallback: '/',
+  templatedUrls: {
+    '/': ['index.html']
+  }
 })
 .then(() => {
   console.log('Service worker generated.');
